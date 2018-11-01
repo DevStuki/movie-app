f948d3995a220d9557bafd96ae7add77bb29f92b
DevStuki <devstuki@gmail.com>
2018-11-01 13:54:37 +0900

    added .prettierrc

--- src/Movie.js
+++ src/Movie.js
@@ -1,7 +1,7 @@
-import React from "react";
-import PropTypes from "prop-types";
-import LinesEllipsis from "react-lines-ellipsis";
-import "./Movie.css";
+import React from 'react';
+import PropTypes from 'prop-types';
+import LinesEllipsis from 'react-lines-ellipsis';
+import './Movie.css';
 
 function Movie({ title, poster, genres, synopsis }) {
   return (
@@ -42,15 +42,15 @@ Movie.propTypes = {
   title: PropTypes.string.isRequired,
   poster: PropTypes.string.isRequired,
   genres: PropTypes.array.isRequired,
-  synopsis: PropTypes.string.isRequired
+  synopsis: PropTypes.string.isRequired,
 };
 
 MoviePoster.propTypes = {
   poster: PropTypes.string.isRequired,
-  alt: PropTypes.string.isRequired
+  alt: PropTypes.string.isRequired,
 };
 MovieGenre.propTypes = {
-  genre: PropTypes.string.isRequired
+  genre: PropTypes.string.isRequired,
 };
 
 export default Movie;
