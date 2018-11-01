4c2f677abe4923dccdbaf174504307138ce0f597
DevStuki <devstuki@gmail.com>
2018-11-01 12:07:39 +0900

    Updates

---
+++ precache-manifest.c7b9c13a68ad731a3d89ea706076f115.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2a4a27000ba76122af42",
+    "url": "/movie-app/static/js/runtime~main.2a4a2700.js"
+  },
+  {
+    "revision": "18583b8f75bcd73ed519",
+    "url": "/movie-app/static/js/main.18583b8f.chunk.js"
+  },
+  {
+    "revision": "392d6e18dd1fb16cb5e2",
+    "url": "/movie-app/static/js/1.392d6e18.chunk.js"
+  },
+  {
+    "revision": "18583b8f75bcd73ed519",
+    "url": "/movie-app/static/css/main.dbc5a2b6.chunk.css"
+  },
+  {
+    "revision": "e46265b5bab4ecc7e0ca683cdf27fc0f",
+    "url": "/movie-app/index.html"
+  }
+];
